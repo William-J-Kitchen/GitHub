3c7da40956d6a63672debb2fe23c58197373f901
William-J-Kitchen <98485895+William-J-Kitchen@users.noreply.github.com>
2022-02-03 12:59:48 -0600

    Bootstrap 4 Project

---
+++ Python/Projects/HelloWorld/app.py
@@ -0,0 +1,74 @@
+#print('A dog')
+#print('o----')
+#print(' ||||')
+#print('*' * 10)
+#print(4 * 5)
+
+#integer = 10
+#float = 4.901
+#string = "Text here"
+#boolean = True
+#print(integer * float)
+
+#patient_name = input('What is your name? ')
+#favorite_color = input ('What is your favorite color? ')
+#print('Hi ' + patient_name + ', I hear your favorite color is ' + favorite_color +'!')
+
+#patient_name = "John Smith"
+#patient_age = 20
+#patient_isnew = True
+
+#print(patient.age)
+
+# birth_year = input('Birth Year: ')
+# print(type(birth_year))
+# age = 2021 - int(birth_year)
+# print(type(birth_year))
+# print(age)
+# type(age)
+
+#weight_lbs = input('What is your weight in pounds? ')
+#weight_kg = int(weight_lbs) * 0.453592
+#print('Your weight in Kilograms is: ' + str(weight_kg) + 'kg')
+
+#longstring = '''
+#Here's a big string.
+#With lines.
+#Etc.
+#Here's Johnny!'''
+#print(longstring)
+#print(longstring[-1]) #square brackets print a character in the string. Letters are numbered in order. Negative starts at the end.
+#print(longstring[0:5]) #returns characters from 0-2, but not the last (5)
+#print(longstring[0:]) #no end index returns all. If first is left blank, it assumes a start of 0.
+#print(longstring[:])
+
+# #John [Smith] is a coder.
+# first = 'John'
+# last = 'Smith'
+# message = first + ' [' + last + '] is a coder.'
+# print(message)
+# msg = f'{first} [{last}] is a coder.'
+# print(msg)
+
+# #String Character Count
+# course = 'Python for Beginners'
+# print(len(course)) #len can be used to count the number of items in a list, as well.
+# #print(course.upper())
+# #print(course.lower())
+# #print(course.find('o')) #returns the index of the first instance of the case-sensitive letter listed. -1 means not found.
+# print(course.replace('Beginners', 'Absolute Beginners'))
+# print(course.title())
+# print('Python' in course) #returns Boolean value for text found in string
+# print(course)
+
+#Arithmetic Operations
+# x = 10
+# print(x)
+# x = x + 3
+# print(x)
+# x += 3
+# print(x)
+# x -= 3
+# print(x)
+x = 10 + 3 * 2
+print(x)
