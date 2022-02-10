b6a4dda5d629690d93ce0dc47671d541b4308787
William-J-Kitchen <98485895+William-J-Kitchen@users.noreply.github.com>
2022-02-10 09:36:03 -0600

    Added comments to JS

--- TechAcademy/JavaScript/Basic JavaScript Projects/Project8_string_methods/JS/main.js
+++ TechAcademy/JavaScript/Basic JavaScript Projects/Project8_string_methods/JS/main.js
@@ -1,4 +1,4 @@
-function full_Sentence() {
+function full_Sentence() { //concatenates the strings in section_1,4
     var section_1 = "This function ";
     var section_2 = "serves the purpose ";
     var section_3 = "of concatenating ";
@@ -7,23 +7,23 @@ function full_Sentence() {
     document.getElementById("Concatenate").innerHTML = complete;
 }
 
-function slice_Method() {
+function slice_Method() { //Slices out characters after the 29th and presents
     var Sentence = "This method will cut out the final section.";
     var Section = Sentence.slice(29);
     document.getElementById("Slice").innerHTML = Section;
 }
 
-function string_Method() {
+function string_Method() { //converts X to a string
     var X = 12;
     document.getElementById("Numbers_to_string").innerHTML = X.toString();
 }
 
-function precision_Method() {
+function precision_Method() { //cuts pi off at the 6th digit
     var X = 3.141592653589793;
     document.getElementById("Precision").innerHTML = X.toPrecision(6);
 }
 
-function fixed_Method() {
+function fixed_Method() { //sets X to be 8 digits in length.
     var X = 546.32
     document.getElementById("Fixed").innerHTML = X.toFixed(8);
 }
