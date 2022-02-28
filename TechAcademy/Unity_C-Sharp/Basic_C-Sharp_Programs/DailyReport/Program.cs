5e620ae9631f36ed978bcfeb7e74b3223be3e8ee
William-J-Kitchen <98485895+William-J-Kitchen@users.noreply.github.com>
2022-02-28 16:02:26 -0600

    C# & Unity Training

---
+++ TechAcademy/Unity_C-Sharp/Basic_C-Sharp_Programs/DailyReport/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy \nStudent Daily Report");
+
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.ReadLine();
+
+            Console.WriteLine("What course are you on?");
+            string course = Console.ReadLine();
+            Console.ReadLine();
+
+            Console.WriteLine("What page number?");
+            string pageNumber = Console.ReadLine();
+            short currentPage = Convert.ToInt16(pageNumber);
+            Console.ReadLine();
+
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false.\"");
+            string needHelp = Console.ReadLine();
+            bool helpNeeded = Convert.ToBoolean(needHelp);
+            Console.ReadLine();
+
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            string positiveExperiences = Console.ReadLine();
+            Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+            Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            string hoursStudied = Console.ReadLine();
+            byte studyHours = Convert.ToByte(hoursStudied);
+            Console.ReadLine();
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+
+        }
+    }
+}
