6d80c0cd4b69a1c573ad08c7da03e4d87f727440
William-J-Kitchen <98485895+William-J-Kitchen@users.noreply.github.com>
2022-03-03 13:08:27 -0600

    Branching statements

--- TechAcademy/Unity_C-Sharp/Basic_C-Sharp_Programs/BasicPractice/ConsoleApp1/Program.cs
+++ TechAcademy/Unity_C-Sharp/Basic_C-Sharp_Programs/BasicPractice/ConsoleApp1/Program.cs
@@ -20,8 +20,17 @@ namespace ConsoleApp1
             //long number = long.Parse(input);
             //Console.WriteLine(number * 50);
 
+            //int num1 = 7;
+            //int num2 = 12;
+            //string result = num1 > num2 ? "num1 is greater than num2" : "num1 is not greater than num2";
+            //Console.WriteLine(result);
 
+            Console.WriteLine("What is your favorite number?");
+            int favNum = Convert.ToInt32(Console.ReadLine());
 
+            string result = favNum == 7 ? "You have an awesome favorite number." : "You do not have an awesome favorite number.";
+            Console.WriteLine(result);
+            Console.ReadLine();
         }
     }
 }
---
+++ TechAcademy/Unity_C-Sharp/Basic_C-Sharp_Programs/BranchingSubmission/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BranchingSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Expresss. Please follow the instructions below.\nPlease enter the package weight:");
+            int Weight = Convert.ToInt32(Console.ReadLine());
+            if (Weight > 50) {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Please enter the package width:");
+            int Width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the package height:");
+            int Height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the package length:");
+            int Length = Convert.ToInt32(Console.ReadLine());
+
+            if (Height + Width + Length > 50) {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                Environment.Exit(0);
+            }
+
+            decimal Total = ((Height * Width * Length) * Weight) / 100;
+            string Quote = Total.ToString("0.00");
+            Console.WriteLine("Your estimated total for shipping this package is: $" + Quote);
+        }
+    }
+}
