5cd016a28bccb99f46fc7557b12096b1da22ef8f
William-J-Kitchen <98485895+William-J-Kitchen@users.noreply.github.com>
2022-03-03 10:08:05 -0600

    C# use of operators for comparison and basic math

---
+++ TechAcademy/Unity_C-Sharp/Basic_C-Sharp_Programs/BasicPractice/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "test";
+            //int number = 8;
+            //string total = number + name;
+            //Console.WriteLine(total);
+
+            //int sum = 5 + 10;
+            //Console.WriteLine("Five plus Ten = " + sum.ToString());
+            //Console.ReadLine();
+
+            Console.WriteLine("Please input a number to be multiplied by 50.");
+            string input = Console.ReadLine();
+            long number = long.Parse(input);
+            Console.WriteLine(number * 50);
+
+        }
+    }
+}
