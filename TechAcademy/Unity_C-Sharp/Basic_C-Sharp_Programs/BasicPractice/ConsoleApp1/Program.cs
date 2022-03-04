a42dcb790c695fcde5ae761d8694695fecff9bcc
William-J-Kitchen <98485895+William-J-Kitchen@users.noreply.github.com>
2022-03-04 13:30:04 -0600

    Arrays & Strings

--- TechAcademy/Unity_C-Sharp/Basic_C-Sharp_Programs/BasicPractice/ConsoleApp1/Program.cs
+++ TechAcademy/Unity_C-Sharp/Basic_C-Sharp_Programs/BasicPractice/ConsoleApp1/Program.cs
@@ -25,38 +25,40 @@ namespace ConsoleApp1
             //string result = num1 > num2 ? "num1 is greater than num2" : "num1 is not greater than num2";
             //Console.WriteLine(result);
 
-            Console.WriteLine("What is your favorite number?");
-            int favNum = Convert.ToInt32(Console.ReadLine());
+			//          Console.WriteLine("What is your favorite number?");
+			//          int favNum = Convert.ToInt32(Console.ReadLine());
 
-            string result = favNum == 7 ? "You have an awesome favorite number." : "You do not have an awesome favorite number.";
-            Console.WriteLine(result);
-            Console.ReadLine();
+			//          string result = favNum == 7 ? "You have an awesome favorite number." : "You do not have an awesome favorite number.";
+			//          Console.WriteLine(result);
+			//          Console.ReadLine();
 			
-			int day = 4;
-		switch (day) 
-		{
-		  case 1:
-			Console.WriteLine("Monday");
-			break;
-		  case 2:
-			Console.WriteLine("Tuesday");
-			break;
-		  case 3:
-			Console.WriteLine("Wednesday");
-			break;
-		  case 4:
-			Console.WriteLine("Thursday");
-			break;
-		  case 5:
-			Console.WriteLine("Friday");
-			break;
-		  case 6:
-			Console.WriteLine("Saturday");
-			break;
-		  case 7:
-			Console.WriteLine("Sunday");
-			break;
-		}
+			//	int day = 4;
+			//switch (day) 
+			//{
+			//  case 1:
+			//	Console.WriteLine("Monday");
+			//	break;
+			//  case 2:
+			//	Console.WriteLine("Tuesday");
+			//	break;
+			//  case 3:
+			//	Console.WriteLine("Wednesday");
+			//	break;
+			//  case 4:
+			//	Console.WriteLine("Thursday");
+			//	break;
+			//  case 5:
+			//	Console.WriteLine("Friday");
+			//	break;
+			//  case 6:
+			//	Console.WriteLine("Saturday");
+			//	break;
+			//  case 7:
+			//	Console.WriteLine("Sunday");
+			//	break;
+			//}
+
+
         }
     }
 }
