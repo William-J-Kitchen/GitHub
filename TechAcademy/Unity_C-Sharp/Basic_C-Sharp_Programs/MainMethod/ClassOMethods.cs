64cc6d432d8e43331171c083179c36d1904f8e0b
William-J-Kitchen <98485895+William-J-Kitchen@users.noreply.github.com>
2022-03-08 12:51:27 -0600

    Updated MainMethods

---
+++ TechAcademy/Unity_C-Sharp/Basic_C-Sharp_Programs/MainMethod/ClassOMethods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public static class ClassOMethods
+{
+    public static int Multiplication(int mult)
+    {
+        return (mult * 3);
+    }
+
+    public static decimal Multiplication(decimal decMult)
+    {
+        int x = Convert.ToInt32(decMult * 3m);
+        return (x);
+    }
+
+    public static int Multiplication(string stringMult)
+    {
+        int y = Convert.ToInt32(stringMult);
+        return (y * 3);
+    }
+}
+
---
+++ TechAcademy/Unity_C-Sharp/Basic_C-Sharp_Programs/MainMethod/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class Program
+{
+    public static int mult = 3;
+    public static decimal decMult = 4;
+    public static string stringMult = "5";
+    public static void Main()
+    {
+
+
+        Console.WriteLine(ClassOMethods.Multiplication(mult));
+        Console.WriteLine(ClassOMethods.Multiplication(decMult));
+        Console.WriteLine(ClassOMethods.Multiplication(stringMult));
+    }
+}
