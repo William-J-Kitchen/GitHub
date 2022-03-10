cf91eeb21ca6d77fad3b9541a099386d4374a310
William-J-Kitchen <98485895+William-J-Kitchen@users.noreply.github.com>
2022-03-10 11:26:52 -0600

    More practice with overloaded methods and classes

---
+++ TechAcademy/Unity_C-Sharp/Basic_C-Sharp_Programs/ClassSubmission/PracticeClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+class PracticeClass
+{
+    public static int y = 0;
+    public static void Division(int userNum)
+    { Console.WriteLine( userNum / 2); }
+
+    public static int Multiplication(int userNum)
+    {
+        return (userNum * 2);
+    }
+
+    public static float Multiplication(float userNum, float defaultNum)
+    {
+        return (userNum * defaultNum);
+    }
+}
+
---
+++ TechAcademy/Unity_C-Sharp/Basic_C-Sharp_Programs/ClassSubmission/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Program
+{
+    public static float defaultNum = 3.5f;
+    public static void Main()
+    {
+        Console.WriteLine("Pick a number.");
+        int userNum = Convert.ToInt32(Console.ReadLine());
+        PracticeClass.Division(userNum);
+        Console.WriteLine(PracticeClass.Multiplication(userNum));
+        Console.WriteLine(PracticeClass.Multiplication(userNum, defaultNum));
+    }
+}
+
