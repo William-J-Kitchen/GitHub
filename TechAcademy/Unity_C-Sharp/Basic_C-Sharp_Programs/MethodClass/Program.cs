a8abe251b5761d467830d273c5011428d7905a7b
William-J-Kitchen <98485895+William-J-Kitchen@users.noreply.github.com>
2022-03-10 10:09:17 -0600

    More Methods & Classes

---
+++ TechAcademy/Unity_C-Sharp/Basic_C-Sharp_Programs/MethodClass/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+public class Program
+{
+    public static int mult = 3;
+    public static int returned = 6;
+    static void Main(string[] args)
+    {
+
+        VoidClass.MathReturned(mult, returned);
+    }
+}
+
