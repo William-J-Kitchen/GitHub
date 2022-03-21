d78301b6a6bd6dfb69f862d6a20704cc93bcde37
William-J-Kitchen <98485895+William-J-Kitchen@users.noreply.github.com>
2022-03-21 13:25:29 -0500

    Practice with DateTime

--- TechAcademy/Unity_C-Sharp/Basic_C-Sharp_Programs/TwentyOne/Card.cs
+++ TechAcademy/Unity_C-Sharp/Basic_C-Sharp_Programs/TwentyOne/Card.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TwentyOne
 {
@@ -10,6 +11,10 @@ namespace TwentyOne
     {
         public Suit Suit { get; set; }
         public Face Face { get; set; }
+        public override string ToString()
+        {
+            return string.Format("{0} of {1}", Face, Suit);
+        }
     }
     public enum Suit
     {
